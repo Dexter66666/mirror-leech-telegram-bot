40c02bc9a38551e2821974847b378b8060faf086
DARKNELARO <91785572+Dexter66666@users.noreply.github.com>
2022-03-28 12:47:10 +0200

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -59,12 +59,12 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Report Group", "https://t.me/+PRRzqHd31XY3ZWZk")
+    buttons.buildbutton("Owner", "https://t.me/XxXXxxX_xXxXx_XxXXxxX")
+    buttons.buildbutton("Dev", "https://t.me/XxXXxxX_xXxXx_XxXXxxX")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
-This bot can mirror all your links to Google Drive!
+Go To Sleep!
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
         sendMarkup(start_string, context.bot, update.message, reply_markup)
